87637dbe6101ec45c198da7eacdcaedcfb003f34
jaasonw <jayywong92@gmail.com>
2019-12-26 03:29:00 -0800

    no comments in output

--- src/ParagraphGenerator.js
+++ src/ParagraphGenerator.js
@@ -5,11 +5,7 @@ var Gender;
     Gender[Gender["Female"] = 1] = "Female";
     Gender[Gender["Other"] = 2] = "Other";
 })(Gender || (Gender = {}));
-var ParagraphGenerator = /** @class */ (function () {
-    /**
-     * @param name Name of the patient
-     * @param gender Gender of the patient
-     */
+var ParagraphGenerator = (function () {
     function ParagraphGenerator(name, gender) {
         this.name = "";
         this.gender = Gender.Male;
@@ -18,17 +14,9 @@ var ParagraphGenerator = /** @class */ (function () {
         this.name = name;
         this.gender = gender;
     }
-    /**
-     * Generates a complete paragraph
-     * @param problem1 the first problem type (blank for random)
-     * @param problem2 the second problem type (blank for random)
-     * @returns a string containing the complete paragraph
-     */
     ParagraphGenerator.prototype.generateParagraph = function (problem1, problem2) {
         if (problem1 === void 0) { problem1 = ""; }
         if (problem2 === void 0) { problem2 = ""; }
-        // if "any" is selected for both problems, we dont want the same type
-        // for both
         if (problem1 == "" && problem2 == "") {
             problem1 = this.getRandom(this.getProblemTypes());
             problem2 = this.getRandom(this.getProblemTypes());
@@ -59,10 +47,6 @@ var ParagraphGenerator = /** @class */ (function () {
         paragraph += this.convertSentence(this.getRandom(this.sentences[4]) + " ");
         return paragraph;
     };
-    /**
-     * Returns a list of available problem types
-     * @returns the list of available problem types
-     */
     ParagraphGenerator.prototype.getProblemTypes = function () {
         var problemTypes = [];
         for (var problemType in this.problems) {
@@ -70,47 +54,24 @@ var ParagraphGenerator = /** @class */ (function () {
         }
         return problemTypes;
     };
-    /**
-     * Loads a json file containing the sentence bank from a url
-     * @param url the url to load from
-     * @returns a promise for the ajax request
-     */
     ParagraphGenerator.prototype.loadSentences = function (url) {
         var _this = this;
         return $.getJSON(url, function (data) {
             _this.sentences = data;
         });
     };
-    /**
-     * Loads a json file containing the problem-treatment bank from a url
-     * @param url the url to load from
-     * @returns a promise for the ajax request
-     */
     ParagraphGenerator.prototype.loadProblemTreatments = function (url) {
         var _this = this;
         return $.getJSON(url, function (data) {
             _this.problems = data;
         });
     };
-    /**
-     * Updates name
-     * @param name
-     */
     ParagraphGenerator.prototype.updateName = function (name) {
         this.name = name;
     };
-    /**
-     * Updates gender
-     * @param gender
-     */
     ParagraphGenerator.prototype.updateGender = function (gender) {
         this.gender = gender;
     };
-    /**
-    * Generates a problem and treatment pair
-    * @param problemType the type of problem-treatment to generate
-    * @returns a string containing the problem and treatment
-    */
     ParagraphGenerator.prototype.generateProblemTreatment = function (problemType) {
         var sentence = "";
         var problem = this.getRandom(this.problems[problemType]["problems"]);
@@ -118,33 +79,17 @@ var ParagraphGenerator = /** @class */ (function () {
         sentence = problem + " " + treatment;
         return sentence;
     };
-    /**
-     * Returns a random element in an array
-     * @param array The array to pick from
-     * @returns an element from the array
-     */
     ParagraphGenerator.prototype.getRandom = function (array) {
         return array[Math.floor((Math.random() * array.length))];
     };
-    /**
-     * Converts the pronouns and fills in the names of a sentence
-     * @param sentence the input sentence
-     * @returns a sentence with the pronouns and _'s replaced
-     */
     ParagraphGenerator.prototype.convertSentence = function (sentence) {
-        // Fill in names
         sentence = sentence.replace(/_/g, this.name);
-        // Pronoun regex patterns (there might be a cleaner way to do this)
-        // Personal pronouns
         var personalPronounsLower = /\bhe\b|\bshe\b|\bthey\b/g;
         var personalPronounsUpper = /\bHe\b|\bShe\b|\bThey\b/g;
-        // Possessive pronouns
         var possesivePronounsLower = /\bhis\b|\bher\b|\btheir\b/g;
         var possesivePronounsUpper = /\bHis\b|\bHer\b|\bTheir\b/g;
-        // Object pronouns
         var objectPronounsLower = /\bhim\b|\bher\b|\bthem\b/g;
         var objectPronounsUpper = /\bHim\b|\bHer\b|\bThem\b/g;
-        // Convert pronouns
         switch (this.gender) {
             case Gender.Male:
                 sentence = sentence.replace(personalPronounsLower, "he");
